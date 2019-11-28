dfb9af99dc64d1233951c95a11214a173fd6c855
qqc <qqcqqc_123@163.com>
2019-11-28 09:05:24 +0800

    "更改"

--- test_one/admin.py
+++ test_one/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from test_one.models import user
+from test_one.models.user import User_info
 
 
-# class Dz_user(admin.ModelAdmin):
-#     list_display = ('id', 'user_id', 'name', 'mobile')
-#
-#     date_hierarchy = 'create_time'
-#     readonly_fields = ('create_time', 'modify_time')
-#
-#
-# admin.site.register(user_info, Dz_user)
+class Dz_user(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'name', 'mobile')
+
+    date_hierarchy = 'create_time'
+    readonly_fields = ('create_time', 'modify_time')
+
+
+admin.site.register(User_info, Dz_user)
